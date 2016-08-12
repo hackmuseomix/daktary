daf3e79f9b4beb1eb26fc0bf8186bb62fae0b962
Thomas WOLFF <thomas.wolff@cpcoop.fr>
2016-08-12 18:09:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ var GH = {}
 GH.SECRET = 'YWU0ZmRkZGFjOTVlZGM1ZTc1MmI3NjRjZTI3Y2UxZGYyMzdmZTdkZg=='
 GH.ID = 'ODUzMjY1YjA5YjBjMjVlOTg2MTQ='
 // Github Owner's
-GH.OWNER = 'multibao'
+GH.OWNER = 'hackmuseomix'
 // Github crew's repository
-GH.CREW = 'organisations'
+GH.CREW = 'museomixpresentation'
 
 var MULTIBAO = {}
-MULTIBAO.UVP1 = 'L\'open bar des pratiques collaboratives'
-MULTIBAO.UVP2 = 'Rédiger et partager vos expériences'
-MULTIBAO.BUTTON1 = 'Commencer ici'
+MULTIBAO.UVP1 = 'Serious hack of MuseoMix'
+MULTIBAO.UVP2 = 'Archipel-technologie multibao.org'
+MULTIBAO.BUTTON1 = 'C\est quoi multibao?'
 MULTIBAO.BUTTON2 = 'Guide d\'utilisation'
